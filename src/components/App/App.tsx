92bd72d3f11f7575994c8a6982d855d0662c5a7a
Arthur Maksimenko <arturmaksimenko784@gmail.com>
2024-01-02 23:09:23 +0300

    first commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.module.css';
+import appStyles from './App.module.css';
+
+function App() {
+  return (
+    <div className={appStyles.app}>
+      REACT-BURGER
+    </div>
+  );
+}
+
+export default App;
